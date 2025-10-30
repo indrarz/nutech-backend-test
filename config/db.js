5fbe64dc904364b647127002687be743646a3ab3
Indra Rizky Gunawan <60083946+indrarz@users.noreply.github.com>
2025-10-30 15:08:06 +0700

    first commit

--- config/db.js
+++ config/db.js
@@ -1,26 +1,15 @@
-const mongoose = require("mongoose");
+const { connect } = require("mongoose");
 
 async function connectDb() {
   try {
-    const uri = process.env.DB_URI;
-    if (!uri) {
-      console.error(
-        "❌ MongoDB URI tidak ditemukan di environment variable (.env)"
-      );
-      process.exit(1);
-    }
-
-    console.log("🟡 Menghubungkan ke MongoDB...");
-    await mongoose.connect(uri, {
+    await connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      tls: true, // gunakan TLS karena MongoDB Atlas biasanya pakai SSL
-      serverSelectionTimeoutMS: 10000, // timeout 10 detik untuk mencegah buffering hang
+      tls: true,
     });
-
-    console.log("✅ MongoDB Connected Successfully!");
+    console.log("MongoDB Connected!");
   } catch (err) {
-    console.error("❌ MongoDB Connection Error:", err.message);
+    console.error(err.message);
     process.exit(1);
   }
 }
